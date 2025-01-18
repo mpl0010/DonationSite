406572c738b46775084f2029b64669112a79cfde
Matthew Lloyd <mplloyd14@gmail.com>
2025-01-17 20:54:58 -0600

    Add primeNG provider and temp theme.

--- web/src/app/app.config.ts
+++ web/src/app/app.config.ts
@@ -2,6 +2,8 @@ import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter, withInMemoryScrolling } from '@angular/router';
 import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { providePrimeNG } from 'primeng/config';
+import Aura from '@primeng/themes/aura'
 
 export const appConfig: ApplicationConfig = {
     providers: [
@@ -10,7 +12,7 @@ export const appConfig: ApplicationConfig = {
         provideAnimationsAsync(),
         providePrimeNG({
             theme: {
-                preset: lara
+                preset: Aura
             }
         })
     ]
