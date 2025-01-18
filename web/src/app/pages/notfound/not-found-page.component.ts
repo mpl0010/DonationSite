7ba3943d246098ab370493b16f0d14f6c94bc2df
Matthew Lloyd <mplloyd14@gmail.com>
2025-01-17 21:00:28 -0600

    Create NotFoundPage component

---
+++ web/src/app/pages/notfound/not-found-page.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-not-found-page',
+  imports: [],
+  template: '<p>not-found-page works!</p>',
+  styleUrl: './not-found-page.component.scss'
+})
+export class NotFoundPageComponent {
+
+}
