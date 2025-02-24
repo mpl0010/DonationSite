fb23a47870fd2abdbe05cd75c6530733726d8105
Matthew Lloyd <mplloyd14@gmail.com>
2025-02-23 22:26:44 -0600

    Change theme coloring

--- web/src/app/themes/default-theme.ts
+++ web/src/app/themes/default-theme.ts
@@ -1,22 +1,61 @@
 import { definePreset, palette } from '@primeng/themes';
 import Aura from '@primeng/themes/aura';
 
-const values = palette('#96B4D1');
+const primaryValues = palette('#2381A0');
+const secondaryValues = palette('#96B4D1')
 
 export const customTheme = definePreset(Aura, {
     semantic: {
         primary: {
-            50: values["50"],
-            100: values["100"],
-            200: values["200"],
-            300: values["300"],
-            400: values["400"],
-            500: values["500"],
-            600: values["600"],
-            700: values["700"],
-            800: values["800"],
-            900: values["900"],
-            950: values["950"]
-        }
+            50: primaryValues["50"],
+            100: primaryValues["100"],
+            200: primaryValues["200"],
+            300: primaryValues["300"],
+            400: primaryValues["400"],
+            500: primaryValues["500"],
+            600: primaryValues["600"],
+            700: primaryValues["700"],
+            800: primaryValues["800"],
+            900: primaryValues["900"],
+            950: primaryValues["950"]
+        },
+        secondary: {
+            50: secondaryValues["50"],
+            100: secondaryValues["100"],
+            200: secondaryValues["200"],
+            300: secondaryValues["300"],
+            400: secondaryValues["400"],
+            500: secondaryValues["500"],
+            600: secondaryValues["600"],
+            700: secondaryValues["700"],
+            800: secondaryValues["800"],
+            900: secondaryValues["900"],
+            950: secondaryValues["950"]
+        },
+        // TODO: Implement color theme for components that will be commonly used.
+        // components: {
+        //     button: {
+        //         colorScheme: {
+        //             light: {
+        //                 root: {
+        //                     background: '{surface.0}',
+        //                     color: '{surface.700}'
+        //                 },
+        //                 subtitle: {
+        //                     color: '{surface.500}'
+        //                 }
+        //             },
+        //             dark: {
+        //                 root: {
+        //                     background: '{surface.900}',
+        //                     color: '{surface.0}'
+        //                 },
+        //                 subtitle: {
+        //                     color: '{surface.400}'
+        //                 }
+        //             }
+        //         }
+        //     }
+        // }
     }
 });
