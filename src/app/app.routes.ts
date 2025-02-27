5b8622c1d31d58e9fa7b907c7e20b7675341aacd
Matthew Lloyd <mplloyd14@gmail.com>
2025-02-26 21:00:04 -0600

    Remove about us route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,7 +2,6 @@ import { Routes } from '@angular/router';
 
 export const routes: Routes = [
     { path: '', loadComponent: () => import('@/pages/landing/landing-page.component').then(p => p.LandingPageComponent) , pathMatch: 'full' },
-    { path: 'aboutus', loadComponent: () => import('@/pages/aboutus/about-us-page.component').then(p => p.AboutUsPageComponent) , pathMatch: 'full' },
     { path: 'notfound', loadComponent: () => import('@/pages/notfound/not-found-page.component').then(p => p.NotFoundPageComponent) },
     { path: '**', redirectTo: '/notfound' }
 ];
