939b736e9aa18987d1d478eca91e9d3d212038c9
Matthew Lloyd <mplloyd14@gmail.com>
2025-03-18 22:06:47 -0500

    Add route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 
 export const routes: Routes = [
     { path: '', loadComponent: () => import('@/pages/landing/landing-page.component').then(p => p.LandingPageComponent) , pathMatch: 'full' },
+    { path: 'allhospitals', loadComponent: () => import('@/pages/hospitals/hospitals-page.component').then(p => p.HospitalsPageComponent) , pathMatch: 'full' },
     { path: 'notfound', loadComponent: () => import('@/pages/notfound/not-found-page.component').then(p => p.NotFoundPageComponent) },
     { path: '**', redirectTo: '/notfound' }
 ];
